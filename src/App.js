984c88807a1f2a42e37e077d856f967e16ca2956
Yan Sidyakin <sidyakinian@gmail.com>
2025-01-11 10:20:56 -0800

    starting project

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import './App.css';
-import TodoList from './components/TodoList';
-import TodoInput from './components/TodoInput';
+import React from 'react';
 
-function App() {  
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem('todos');
-    return savedTodos ? JSON.parse(savedTodos) : [
-      { id: uuidv4(), task: 'Task 1: Learn React' },
-      { id: uuidv4(), task: 'Task 2: Get a job' },
-      { id: uuidv4(), task: 'Task 3: Enjoy life' },
-    ];
-  });
-  const [newTodo, setNewTodo] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  useEffect(() => {
-    console.log('first load');
-  }, []);
-
-  const addTodo = () => {
-    if (newTodo.trim()) {
-      setTodos([...todos, { id: uuidv4(), task: newTodo }]);
-      setNewTodo('');
-    }
-  };
-
-  const removeTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
+const App = () => {
   return (
-    <div className="todo-list">
-      <h2>Todo list</h2>
-      <TodoInput newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
-      <TodoList todos={todos} removeTodo={removeTodo} />
+    <div>
+      <ul>
+        <li style={{ color: 'white' }}>
+          Learn React
+          <img 
+            src="./trash.svg" 
+            alt="Trash" 
+            style={{ 
+              marginLeft: '10px', 
+              width: '16px', 
+              height: '16px', 
+              cursor: 'pointer' 
+            }} 
+        /></li>
+      </ul>
     </div>
   );
 }
